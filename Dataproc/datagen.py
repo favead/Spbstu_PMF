8712dd811bbb1ec3d06f5691313b41aa6e55ab52
favead <diablo3_monk@mail.ru>
2023-03-11 22:03:57 +0300

    Add&Change(Dataproc): Добавил функцию сохранения сетки в файл и поменял реализацию создания сетки

--- Dataproc/datagen.py
+++ Dataproc/datagen.py
@@ -1,12 +1,16 @@
-from typing import Tuple, List, Union
 import numpy as np
+import h5py
 
 
-def generate_data(mesh_info: List[Tuple[Union[int, float], Union[int, float], Union[int, float]]]) -> np.ndarray:
-    """
-	Генерация данных из гиперкуба с помощью pyDOE lhs
-    input: mesh_info - Лист кортежей вида (min, max, ndots).
-    Первый индекс массива - ось x, второй - ось y, третий - ось z.
-    Следовательно, нужна обработка всех случаев длинны массива
-    """
-    pass
+def generate_data(n_points: int, mesh_dim: int) -> np.ndarray:
+    lower_limits = np.arange(0, n_points) / n_points
+    upper_limits = np.arange(1, 1 + n_points) / n_points
+    points = np.random.uniform(low=lower_limits, high=upper_limits, size=[mesh_dim, n_points]).T
+    np.random.shuffle(points[:, 1])
+    return points
+
+
+def save_mesh_to_file(point_vectors: np.ndarray, filename: str = 'mesh.h5') -> None:
+    f = h5py.File(filename)
+    f.create_dataset("x", data=point_vectors)
+    return None
